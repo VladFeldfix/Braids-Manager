9d7ed0c3649e6a4ea69fbe90107980eda9d97106
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2023-06-04 15:21:15 +0300

    Add files via upload

---
+++ v4/Braids-Manager.py
@@ -0,0 +1,91 @@
+from SmartConsole import *
+
+class main:
+    # constructor
+    def __init__(self):
+        # load smart console
+        self.sc = SmartConsole("Braids Manager", "4.0")
+
+        # set-up main memu
+        self.sc.main_menu["ADD NEW BRAID"] = self.new
+
+        # get settings
+        self.path_main = self.sc.get_setting("Braids Folder")
+
+        # test all paths
+        self.sc.test_path(self.path_main)
+
+        # load databases
+        self.load_databases()
+
+        # display main menu
+        self.sc.start()
+
+    def load_databases(self):
+        # get current braid number
+        self.BRAIDS = 0 
+        for path, dirs, files in os.walk(self.path_main):
+            for file in files:
+                if ".csv" in file:
+                    newid = file.split(".")
+                    newid = file[0]
+                    try:
+                        newid = int(newid)
+                        if newid > self.BRAIDS:
+                            self.BRAIDS = newid
+                    except:
+                        pass
+        
+        self.BRAIDS += 1
+
+    def new(self):
+        # load if number
+        self.load_databases()
+
+        # get number of mpt side plugs
+        global_points = self.sc.input("Insert number of MPT-SIDE PLUGS [1 Plug = 50 Global points]")
+
+        # test user input
+        try:
+            global_points = int(global_points)
+            global_points = global_points*50
+        except:
+            self.sc.error("Invalid input")
+            self.sc.restart()
+            return
+        
+        # get product side number of plugs
+        plugs = self.sc.input("Insert number of PRODUCT-SIDE PLUGS")
+
+        # test user input
+        try:
+            plugs = int(plugs)
+        except:
+            self.sc.error("Invalid input")
+            self.sc.restart()
+            return
+        
+        # for every plug
+        plugs_info = {}
+        for i in range(1, plugs+1):
+            part_number = self.sc.input("For PLUG #"+str(self.BRAIDS)+"."+str(i)+" Insert product-side-plug PART NUMBER").upper() or "None"
+            rafael_part_number = self.sc.input("For PLUG #"+str(self.BRAIDS)+"."+str(i)+" Insert product-side-plug RAFAEL PART NUMBER").upper() or "None"
+            pin_type = self.sc.choose("For PLUG #"+str(self.BRAIDS)+"."+str(i)+" Choose Insert product-side-plug PIN TYPE", ["PIN", "SOCKET", "BOTH"])
+            plugs_info[i] = [str(i), part_number, rafael_part_number, pin_type]
+        
+        # make a map
+        file = open(self.path_main+"/"+str(self.BRAIDS)+".csv", 'w')
+        file.write("GLOBAL POINT,PLUG,PIN,PLUG NUMBER,PART NUMBER,RAFAEL PART NUMBER,PIN TYPE\n")
+        for i in range(1,global_points+1):
+            if i in plugs_info:
+                info = plugs_info[i]
+            else:
+                info = ["","","",""]
+            file.write(str(i)+",,,"+info[0]+","+info[1]+","+info[2]+","+info[3]+"\n")
+        file.close()
+        os.popen(self.path_main+"/"+str(self.BRAIDS)+".csv")
+        
+        # restart
+        self.sc.restart()
+
+main()
