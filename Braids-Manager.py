4a10542023bc711c299016f33348cdfa44f4cc0d
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-02-29 14:26:51 +0200

    Add files via upload

--- Braids-Manager.py
+++ Braids-Manager.py
@@ -9,6 +9,7 @@ class main:
 
         # set-up main memu
         self.sc.add_main_menu_item("ADD NEW BRAID", self.new)
+        self.sc.add_main_menu_item("LUA TO CSV", self.translate)
 
         # get settings
         self.path_main = self.sc.get_setting("Braids Folder")
@@ -105,4 +106,114 @@ class main:
         # restart
         self.sc.restart()
 
+    def translate(self):
+        # get file path
+        path_to_lua = self.sc.input("Insert path to .lua file you wish to convert to .csv")
+        self.sc.test_path(path_to_lua)
+
+        lua = open(path_to_lua,'r')
+        lines = lua.readlines()
+        lua.close()
+
+        # read file data
+        data = {}
+        connector = ""
+        status = None
+        for line in lines:
+            line = line.replace("\n", "")
+            
+            if status == None:
+                if "connector_list" in line:
+                    status = "get_branch_id"
+            
+            if status == "get_branch_id":
+                if "name" in line:
+                    line = line.split("[[")
+                    line = line[1]
+                    line = line.replace("]]", "")
+                    line = line.replace(",", "")
+                    line = line.split("_")
+                    connector = line[2]
+                    data[connector] = []
+                    status = "wait_for_pins_command"
+            
+            if status == "wait_for_pins_command":
+                if "pins" in line:
+                    status = "collect_pins"
+            
+            if status == "collect_pins":
+                if "name" in line and not "kelvin" in line:
+                    if "{" in line:
+                        line = line.split("{")
+                        line = line[1]
+                        line = line.split(",")
+                        global_point = line[0]
+                        pin = line[1]
+                        pin = pin.replace("name","").replace(" ","").replace("=","").replace("[","").replace("]","").replace(",","").replace("}","")
+                        data[connector].append((global_point, pin))
+                    else:
+                        line = line.split("[[")
+                        line = line[1]
+                        line = line.replace("]]", "")
+                        line = line.replace(",", "")
+                        line = line.split("_")
+                        connector = line[2]
+                        data[connector] = []
+                        status = "wait_for_pins_command"
+                if "name" in line and "kelvin" in line:
+                    line = line.replace("name","").replace(" ","").replace("=","").replace("[","").replace("]","").replace("}","").replace("{","").replace("kelvin","")
+                    line = line.split(",")
+                    global_point = line[0]
+                    pin = line[1]
+                    data[connector].append((global_point, pin))
+                    global_point = line[2]
+                    data[connector].append((global_point, pin))
+        
+        # oragnize by global points
+        size = int(global_point) / 50
+        size = int(size)+1
+        #print(size)
+        #print(data)
+        data_by_global_point = {} # GLOBAL POINT, PLUG, PIN, PLUG NUMBER, PART NUMBER, RAFAEL PART NUMBER,PIN TYPE
+        for plug, pins in data.items():
+            for pin in pins:
+                data_by_global_point[pin[0]] = [pin[0], plug, pin[1], "", "", "", "",]
+        
+        for g in range(size):
+            for gl in range(50):
+                gl += g*50
+                gl += 1
+                if not str(gl) in data_by_global_point:
+                    data_by_global_point[str(gl)] = [str(gl),"","","","","",""]
+        #print(data_by_global_point)
+        i = 0
+        for connector in data.keys():
+            i += 1
+            ii = str(i)
+            data_by_global_point[ii][3] = connector
+            data_by_global_point[ii][4] = "None"
+            data_by_global_point[ii][5] = "None"
+            data_by_global_point[ii][6] = "NA"
+        #print(data_by_global_point)
+        sorted_data = []
+        for pins in data_by_global_point.values():
+            sorted_data.append((pins[0], pins[1], pins[2], pins[3], pins[4], pins[5], pins[6]))
+        self.load_databases()
+        filename = self.path_main+"/___test R1_"+str(self.BRAIDS)+".csv"
+        file = open(filename, 'w')
+        file.write("GLOBAL POINT,PLUG,PIN,PLUG NUMBER,PART NUMBER,RAFAEL PART NUMBER,PIN TYPE\n")
+        for line in sorted_data:
+            GLOBAL_POINT = line[0]
+            PLUG = line[1]
+            PIN = line[2]
+            PLUGNUMBER = line[3]
+            PARTNUMBER = line[4]
+            RAFAELPARTNUMBER = line[5]
+            PINTYPE = line[6]
+            file.write(GLOBAL_POINT+","+PLUG+","+PIN+","+PLUGNUMBER+","+PARTNUMBER+","+RAFAELPARTNUMBER+","+PINTYPE+"\n")
+        file.close()
+        os.popen(filename)
+        
+        # restart
+        self.sc.restart()
 main()
